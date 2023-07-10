081e1a741b2200f96f26e443fcaf76a3d179f009
choiruladamm <choiruladamm@gmail.com>
2023-07-10 17:03:59 +0700

    Create Users Route /api/users/create

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,24 @@
-import { Request, Response } from "express"
-import prisma from "../services/prisma"
+import { Request, Response } from "express";
+import prisma from "../services/prisma";
 
 export const userController = {
+  // get all data users
   async index(req: Request, res: Response) {
-    const users = await prisma.user.findMany()
+    const users = await prisma.user.findMany();
 
-    return res.json(users)
-  }
-}
+    return res.json(users);
+  },
+
+  // create user
+  async createUser(req: Request, res: Response) {
+    const userData = req.body;
+    const user = await prisma.user.create({
+      data: {
+        fname: userData.fname,
+        lname: userData.lname
+      },
+    });
+
+    return res.json({ user: user });
+  },
+};
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,5 +4,6 @@ import { userController } from "../controllers/user.controller";
 const routes = Router();
 
 routes.get("/", userController.index);
+routes.post("/create", userController.createUser)
 
 export default routes;
