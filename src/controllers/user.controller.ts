6af1c5ac4b7c5da13ff52b0faf8421ca99e576cc
choiruladamm <choiruladamm@gmail.com>
2023-07-10 17:12:20 +0700

    Add Get One User

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -15,10 +15,25 @@ export const userController = {
     const user = await prisma.user.create({
       data: {
         fname: userData.fname,
-        lname: userData.lname
+        lname: userData.lname,
+      },
+      include: {
+        car: true,
       },
     });
 
     return res.json({ user: user });
+  },
+
+  // find one user
+  async findUniqueUser(req: Request, res: Response) {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({
+      where: {
+        id: String(id),
+      },
+    });
+
+    return res.json({ user });
   },
 };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,6 +4,7 @@ import { userController } from "../controllers/user.controller";
 const routes = Router();
 
 routes.get("/", userController.index);
-routes.post("/create", userController.createUser)
+routes.post("/create", userController.createUser);
+routes.get("/:id", userController.findUniqueUser);
 
 export default routes;
